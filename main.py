3fb037efcddce599d938091c41391af26a7d560f
Deniz Gedik <deniizgedik@gmail.com>
2022-08-14 14:30:11 +0300

    pyperclip functionality restored

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-# import pyperclip
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -27,7 +27,7 @@ def password_generator():
     password = "".join(password_list)
 
     password_input.insert(0, password)
-    # pyperclip.copy(password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
